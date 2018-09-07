3aa62d3d19226fca0f3fd79eca73ad5c0123c519
tvolpert <timothyvolpert@gmail.com>
2018-09-07 15:26:19 -0500

    Agency list is currently working!
    only displays ~10 items, and loads after the rest of the page, but dangit it's working

--- kgiscript.js
+++ kgiscript.js
@@ -1,26 +1,31 @@
-var collID;
+(function() { //begin namespace
 
-document.addEventListener('cdm-collection-landing-page:ready', function(e){
-    // e is instance of CustomEvent
-    console.log(e.detail.collectionId); // {collectionId: '...'}
-	collID = e.detail.collectionId;
-		fetch('https://cdm16884.contentdm.oclc.org/digital/api/search/collection/'+collID)
-	.then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-   for (i = 0 ; i < data.items.length; i++) {
-	   console.log (data.items[i].title +': '+  data.items[i].itemLink)
-   }
+//#AgencyList generate list of agency documents on each agency landing page {
+	var collID; 
+	var listResults = '<div id="pub-list"><h1>Agency Publications in KGI</h1><ul class="agency-pubs">';
+	var resultTarget;
+	var recURL;
+	document.addEventListener('cdm-collection-landing-page:ready', function(e){
+			collID = e.detail.collectionId; //grab collection id from the current page
+			fetch('https://cdm16884.contentdm.oclc.org/digital/api/search/collection/'+collID)
+		.then(function(response) {
+		return response.json();
+	  }) //end .then
+	  .then(function(data) {
+		  for (i = 0 ; i < data.items.length; i++) { //begin loop
+		  var regex = /\/singleitem/;
+		  recURL = data.items[i].itemLink.split(regex)[1];
+		  listResults += '<li><a href="/digital'+recURL+'">'+data.items[i].title+'</a></li>'
+	   } //end loop
 	
-	})
-    // ...
-});
+		  resultTarget = document.querySelector('.CollectionLanding-maincontentLanding');
+		resultTarget.innerHTML += listResults+'</ul></div>';
+		}) //end .then
+	 
+	}); //end :ready event listener
+
+// } end #AgencyList
 
 
-document.addEventListener('cdm-app:ready', function(e){
-    // application ready to use 
-    // e is instance of CustomEvent 
-	console.log('hmmm...');
-});
 
+})(); //end namespace
