549049fce1fe745daa0ce72f2a6b2361ed3782e3
tvolpert <timothyvolpert@gmail.com>
2018-09-21 09:09:10 -0500

    rebuild complete i think

--- kgiscript.js
+++ kgiscript.js
@@ -1,14 +1,24 @@
-(function() { //begin namespace
 
-var docLists = document.querySelectorAll('.agency-docs')
+(function(){console.log('this worked');
+
 	function expandAll() {
+		let docLists = document.querySelectorAll('.agency-container');
 		docLists.forEach(function(item) {
 			item.classList.add('open');
 		})
-	}
+	};
 	function collapseAll() {
+		let docLists = document.querySelectorAll('.agency-container');
 		docLists.forEach(function(item) {
 			item.classList.remove('open');
+			if (location.hash) {
+				location.hash = '';
+			}
 		})
 	}
-}()//end namespace
+	document.addEventListener('DOMContentLoaded', function() {
+		document.querySelector('#expandAll').addEventListener('click', expandAll);
+		document.querySelector('#collapseAll').addEventListener('click', collapseAll);
+	});
+	
+})();
